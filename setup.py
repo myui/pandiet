486c82db8907470713c09e8857345eb5bd95bcf3
Makoto Yui <myui@apache.org>
2021-12-10 08:27:21 +0900

    Bumped up to v0.1.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def parse_requirements(filename):
 
 setuptools.setup(
     name="pandiet",
-    version="0.1.4",
+    version="0.1.5",
     author="Makoto Yui",
     author_email="myui@apache.org",
     license = "BSD-3-Clause",
