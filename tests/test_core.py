3a985c79b85dd051b6cf5cd498ace2a17128f281
Makoto Yui <myui@apache.org>
2023-12-12 19:48:44 +0900

    Add test for inplace true option

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,3 +5,9 @@ def test_reducer():
   df = pd.read_csv('https://raw.githubusercontent.com/bundgus/pydata2parquet/master/Most-Recent-Cohorts-Scorecard-Elements.csv')
   df_reduced = Reducer().reduce(df, verbose=False)
   assert df_reduced.memory_usage().sum() < df.memory_usage().sum()
+
+def test_reducer_inplace():
+  df = pd.read_csv('https://raw.githubusercontent.com/bundgus/pydata2parquet/master/Most-Recent-Cohorts-Scorecard-Elements.csv')
+  orig_size = df.memory_usage().sum()
+  df_reduced = Reducer().reduce(df, verbose=False, inplace=True)
+  assert df_reduced.memory_usage().sum() < orig_size
